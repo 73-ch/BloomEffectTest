dac51dbc06118df993bc34b84709af76ccfe109c
634nami <cigarbox.nami@gmail.com>
2019-12-19 04:00:41 +0900

    細かな調整

--- bin/data/shaders/blend.frag
+++ bin/data/shaders/blend.frag
@@ -1,6 +1,7 @@
 #version 410
 
 uniform vec2 u_resolution;
+uniform vec2 shrink_resolution;
 
 uniform sampler2D input1;
 uniform sampler2D input2;
@@ -8,12 +9,17 @@ uniform sampler2D input2;
 out vec4 out_color;
 
 void main() {
-    vec4 color;
+    vec3 color;
 
     vec2 st = gl_FragCoord.xy / u_resolution;
 
-    color = texture(input1, st);
-    color += texture(input2, st);
+    color = texture(input1, st).rgb;
+    color += texture(input2, st).rgb;
 
-    out_color = color;
+    // gamma
+    // color = vec3(1.) - exp(-color);
+    // color = pow(color, vec3(0.45454545454545453));
+
+    out_color = vec4(color, 1.0);
+
 }
--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -9,14 +9,21 @@ void ofApp::setup(){
     light_shader.load("shaders/default.vert", "shaders/light.frag");
 
     extract_shader.load("shaders/fboplane.vert", "shaders/extract.frag");
-    bloom_shader.load("shaders/fboplane.vert", "shaders/bloom.frag");
+    blur_shader.load("shaders/fboplane.vert", "shaders/blur.frag");
     blend_shader.load("shaders/fboplane.vert", "shaders/blend.frag");
 
     light_direction = glm::vec3(-0.5, 1.0, 0.5);
     windowResized(ofGetWidth(), ofGetHeight());
 
 
+    box.set(100);
 
+    box.enableColors();
+    auto &m = box.getMesh();
+    vector<ofFloatColor> colors(m.getNumVertices(), ofFloatColor(0.2, 0.5,0.5,1.0));
+    m.addColors(colors);
+
+    
 }
 
 //--------------------------------------------------------------
@@ -40,7 +47,7 @@ void ofApp::draw(){
     light_shader.setUniform4f("ambient_color", glm::vec4(0.,0.1,0.1,1.0));
 
     ofSetColor(255, 200);
-    ofDrawBox(glm::vec3(0), 100);
+    box.draw();
 
     light_shader.end();
 
@@ -60,56 +67,57 @@ void ofApp::draw(){
     extract_shader.setUniformTexture("input_texture", default_fbo.getTexture(), 0);
 
     extract_shader.setUniform2f("u_resolution", glm::vec2(src_fbo.getWidth(), src_fbo.getHeight()));
-    ofDrawPlane(0, 0, ofGetWidth() * 2., ofGetHeight() * 2.);
+    ofDrawPlane(0, 0, dst_fbo.getWidth() * 2., dst_fbo.getWidth() * 2.);
 
     extract_shader.end();
     dst_fbo.end();
 
     swap(dst_fbo, src_fbo);
     
+    for (int i = 0; i < blur_count; i++) {
+        // 縦方向のぼかし
+        dst_fbo.begin();
+        ofClear(0);
+        blur_shader.begin();
+        blur_shader.setUniform2f("u_resolution", glm::vec2(src_fbo.getWidth(), src_fbo.getHeight()));
 
-    // 縦方向のぼかし
-    dst_fbo.begin();
-    ofClear(0);
-    bloom_shader.begin();
-    bloom_shader.setUniform2f("u_resolution", glm::vec2(src_fbo.getWidth(), src_fbo.getHeight()));
+        // blur_shader.setUniform1fv("offsets_v", &offset_v[0], offset_v.size());
+        // blur_shader.setUniform1fv("weights_v", &weight_v[0], weight_v.size());
+        
+        blur_shader.setUniformTexture("input_texture", src_fbo.getTexture(), 0);
+        blur_shader.setUniform1i("is_vertical", true);
 
-    // bloom_shader.setUniform1fv("offsets_v", &offset_v[0], offset_v.size());
-    // bloom_shader.setUniform1fv("weights_v", &weight_v[0], weight_v.size());
-    
-    bloom_shader.setUniformTexture("input_texture", src_fbo.getTexture(), 0);
-    bloom_shader.setUniform1i("is_vertical", true);
+        ofDrawPlane(0, 0, dst_fbo.getWidth() * 2., dst_fbo.getWidth() * 2.);
 
-    ofDrawPlane(0, 0, ofGetWidth() * 2., ofGetHeight() * 2.);
+        dst_fbo.end();
 
-    dst_fbo.end();
+        swap(dst_fbo, src_fbo);
 
-    swap(dst_fbo, src_fbo);
+        // 横方向へのぼかし
+        dst_fbo.begin();
+        ofClear(0);
 
+        blur_shader.setUniform1fv("offsets_h", &offset_h[0], offset_h.size());
+        blur_shader.setUniform1fv("weights_h", &weight_h[0], weight_h.size());
 
-    // 横方向へのぼかし
-    dst_fbo.begin();
-    ofClear(0);
+        blur_shader.setUniformTexture("input_texture", src_fbo.getTexture(), 0);
+        blur_shader.setUniform1i("is_vertical", false);
 
-    bloom_shader.setUniform1fv("offsets_h", &offset_h[0], offset_h.size());
-    bloom_shader.setUniform1fv("weights_h", &weight_h[0], weight_h.size());
+        ofDrawPlane(0, 0, dst_fbo.getWidth() * 2., dst_fbo.getWidth() * 2.);
 
-    bloom_shader.setUniformTexture("input_texture", src_fbo.getTexture(), 0);
-    bloom_shader.setUniform1i("is_vertical", false);
+        blur_shader.end();
+        dst_fbo.end();
 
-    ofDrawPlane(0, 0, ofGetWidth() * 2., ofGetHeight() * 2.);
-
-    bloom_shader.end();
-    dst_fbo.end();
-
-    swap(dst_fbo, src_fbo);
+        swap(dst_fbo, src_fbo);
+    }
+   
 
     // 最終的に元のシーンと加算合成
 
     if (enable) {
         blend_shader.begin();
 
-        blend_shader.setUniform2f("u_resolution", glm::vec2(src_fbo.getWidth(), src_fbo.getHeight()));
+        blend_shader.setUniform2f("u_resolution", glm::vec2(ofGetWidth(), ofGetHeight()));
         blend_shader.setUniformTexture("input1", default_fbo, 0);
         
         blend_shader.setUniformTexture("input2", src_fbo, 1);    
@@ -122,6 +130,8 @@ void ofApp::draw(){
     } else {
         default_fbo.draw(0,0);
     }
+
+    // src_fbo.draw(0,0,ofGetWidth() / 6, ofGetHeight() / 6.);
     
 
     // fbo.draw(0,0);
@@ -180,11 +190,14 @@ void ofApp::windowResized(int w, int h){
     settings.maxFilter = GL_LINEAR;
     settings.minFilter = GL_LINEAR;
     settings.internalformat = GL_RGBA;
- 
-    dst_fbo.allocate(ofGetWidth(), ofGetHeight());
-    src_fbo.allocate(ofGetWidth(), ofGetHeight());
+
+    dst_fbo.allocate(ofGetWidth() / shrink_size, ofGetHeight() / shrink_size);
+    src_fbo.allocate(ofGetWidth() / shrink_size , ofGetHeight() / shrink_size);
     dst_fbo.getTexture().getTextureData().bFlipTexture = true;
     src_fbo.getTexture().getTextureData().bFlipTexture = true;
+
+    dst_fbo.getTexture().setTextureMinMagFilter(GL_LINEAR, GL_LINEAR);
+    src_fbo.getTexture().setTextureMinMagFilter(GL_LINEAR, GL_LINEAR);
 
     default_fbo.allocate(ofGetWidth(), ofGetHeight());
     default_fbo.getTexture().getTextureData().bFlipTexture = true;
